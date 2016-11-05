2ccda68ffe9f21ac1d2f183188f989fb9e8af4ab
bigzhao <350490072@qq.com>
2016-11-05 22:27:28 +0800

    Added some comments and adjusted some code to make it more readable

--- fea.c
+++ fea.c
@@ -6,7 +6,7 @@
 #include "math.h"
 
 
-struct Particle
+typedef struct Particle
 {
 	int size;
 	double fitness;
@@ -14,30 +14,20 @@ struct Particle
 	double position[L+1];
 	double velocity[L+1];
 	double best_position[L+1];
-};
+}Particle;
 
-
-struct SubPopulation
+ 
+typedef struct SubPopulation
 {
-	struct Particle swarm[NUM_PARTICLE];
+	Particle swarm[NUM_PARTICLE];
 	double best_global_position[L+1];
 	double best_global_fitness;
-}sub_populations[SUBPOP_LEN];
+}SubPopulation;
+SubPopulation sub_populations[SUBPOP_LEN];
 
 
-double object_function1(double x[])
-{
-	int i;
-    double sum = 0;
-	for(i = 0;i < NUM; i++)
-	{
-		sum += (x[i] * x[i]);
-	}
-    return sum;
-}
-
-
-double object_function(double x[])
+// Griewank 函数
+double object_function_1(double x[])
 {
 	double part1 = 0, part2 = 1;
 	int i;
@@ -51,56 +41,132 @@ double object_function(double x[])
 }
 
 
-void make_array_random(int a[], int n)
+// Ackleys Function
+double object_function(double x[])
+{
+	double first_part = 0, second_part = 0;
+	int i;
+	for (i = 0; i < NUM; i++)
+	{
+		first_part += x[i] * x[i];
+		second_part += cos(2 * PI * x[i]);
+	}
+	return -20 * exp(-0.2 * sqrt(first_part / NUM)) - exp(second_part / NUM) + 20 + 2.718281;
+}
+
+
+/************************************************************************
+* Function Name : make_array_random
+* Create Date : 2016/11/04
+* Author/Corporation : bigzhao
+**
+Description : 将一个长度为n的数组打乱顺序
+*
+* Param : array: 需要被打乱的数组
+* length 数组的长度
+**
+************************************************************************/
+void make_array_random(int array[], int length)
 {
 	int index, tmp, i;
 	srand(time(NULL));
-	for (i = 0; i <n; i++)
+	for (i = 0; i <length; i++)
 	{
-		index = rand() % (n - i) + i;
+		index = rand() % (length - i) + i;
 		if (index != i)
 		{
-			tmp = a[i];
-			a[i] = a[index];
-			a[index] = tmp;
+			tmp = array[i];
+			array[i] = array[index];
+			array[index] = tmp;
 		}
 	}
 }
 
 
-int judge_in_array(int element, int array[], int length)    //judge a element whether in sub_index, if in return 1, else return 0 
+/************************************************************************
+* Function Name : judge_in_array
+* Create Date : 2016/11/04
+* Author/Corporation : bigzhao
+**
+Description : 判断元素是否在数组里面，在则返回1 ，不在返回0
+利用array是有序的数组这个特性，使用二分查找
+*
+* Param : element: 被判断的元素
+* array: 被判断的数组
+* length： 数组的长度
+**
+************************************************************************/
+int judge_in_array(int element, int array[], int length)
 {
-	int i;
-	for(i = 0; i < length; i++)
+
+	int low, high, mid;
+
+	low = 0;
+	high = length - 1;
+
+	while (low <= high)
 	{
-		if(element == array[i])
+		mid = (low + high) / 2;
+		if (array[mid] == element) 
+		{
 			return 1;
+		}
+		else if (array[mid] < element) 
+		{
+			low = mid + 1;
+		}	
+		else 
+		{
+			high = mid - 1;
+		}
 	}
-	return 0;
+
+	return 0; 
 }
 
-/* 
 
-:param sub_index:
-
-*/
-
-void rebulid(int single_index[], int index_length, double sub_solution[], double global_solution[], double rebulid_solution[]) //rebulit solution
+/************************************************************************
+* Function Name : rebulid
+* Create Date : 2016/11/04
+* Author/Corporation : bigzhao
+**
+Description : 重建维度为NUM的解，用途是亚种群也能计算出适应度，亚种群没有的维度去全局解中拿
+*
+* Param : index: 亚种群对应全局的下标数组
+* index_length: 下标数组的长度
+* sub_solution 亚种群的部分解
+* global_solution： NUM维度的全局解
+* rebulid_solution: 局部解和全局解的结合
+**
+************************************************************************/
+void rebulid(int index[], int index_length, double sub_solution[], double global_solution[], double rebulid_solution[])
 {
-	int i, index, j = 0;
+	int i, j = 0;
 	// printf("%d\n", index_length);
 
-	for(i = 0; i < index_length; i++)
+	for (i = 0; i < index_length; i++)
 	{
 
-		rebulid_solution[single_index[i]] = sub_solution[j++];
+		rebulid_solution[index[i]] = sub_solution[j++];
 
 	}
 
 }
 
 
-void initialize_subpops(int sub_index[SUBPOP_LEN][L + 1], struct SubPopulation sub_populations[], double global_solution[])
+/************************************************************************
+* Function Name : initialize_subpops
+* Create Date : 2016/11/04
+* Author/Corporation : bigzhao
+**
+Description : 亚种群初始化，这里用的是CS-L的分解结构，即{X1,X2,...,XL+1},{X2,...,XL+2}
+*
+* Param : sub_index: 一个SUBPOP_LEN × （L + 1）的二维数组，用来存放每个亚种群对应的下标
+* sub_populations: 结构体数组，存放输出的亚种群
+* global_solution： NUM维度的全局解，用来计算适应度
+**
+************************************************************************/
+void initialize_subpops(int sub_index[SUBPOP_LEN][L + 1], SubPopulation sub_populations[], double global_solution[])
 {
 	int i, j, k;
 
@@ -110,10 +176,10 @@ void initialize_subpops(int sub_index[SUBPOP_LEN][L + 1], struct SubPopulation s
 	double fitness;
 	double rebulid_solution[NUM];
 
-	for(i = 0; i < SUBPOP_LEN; i++)
+	for (i = 0; i < SUBPOP_LEN; i++)
 	{
 		printf("the %dth subgroup: ", i+1);
-		for(j = 0; j < L + 1; j++)
+		for (j = 0; j < L + 1; j++)
 		{
 			sub_index[i][j] = i + j;
 			printf("%d ", sub_index[i][j]);
@@ -127,9 +193,9 @@ void initialize_subpops(int sub_index[SUBPOP_LEN][L + 1], struct SubPopulation s
 	{
 		best_global_fitness = 1.0e308;
 		// initialize paticle
-		for(j = 0;j < NUM_PARTICLE; j++)
+		for (j = 0;j < NUM_PARTICLE; j++)
 		{
-			for(k = 0; k < L + 1; k++)
+			for (k = 0; k < L + 1; k++)
 			{
 				random_position[k] = (MAX_X - MIN_X) * rand() / (double)RAND_MAX  + MIN_X;
 				random_velocity[k] = (MAX_V - MIN_V) * rand() / (double)RAND_MAX  + MIN_V;
@@ -139,7 +205,6 @@ void initialize_subpops(int sub_index[SUBPOP_LEN][L + 1], struct SubPopulation s
 			fitness = object_function(rebulid_solution);
 			// struct Particle p = {L + 1,fitness, fitness, *random_position, *random_velocity, *random_position};
 			
-			// sub_populations[i].swarm[j] = p;
 			sub_populations[i].swarm[j].size = L + 1;
 			sub_populations[i].swarm[j].fitness = fitness;
 			sub_populations[i].swarm[j].best_fitness = fitness;
@@ -147,27 +212,34 @@ void initialize_subpops(int sub_index[SUBPOP_LEN][L + 1], struct SubPopulation s
 			memcpy(sub_populations[i].swarm[j].best_position, random_position, sizeof(double)*(L+1));
 			memcpy(sub_populations[i].swarm[j].velocity, random_velocity, sizeof(double)*(L+1));
 
-
-			if(sub_populations[i].swarm[j].fitness < best_global_fitness)
+			if (sub_populations[i].swarm[j].fitness < best_global_fitness)
 			{
-				// printf("\n");
-				// printf("xiaoyule \n");s
 				best_global_fitness = sub_populations[i].swarm[j].fitness;
 				for (k = 0; k < L+1; k++)
 				{
 					sub_populations[i].best_global_position[k] = sub_populations[i].swarm[j].position[k];
 				}
-				// printf("%lf, %lf\n", sub_populations[i].swarm[j].fitness, sub_populations[i].best_global_fitness);
-				// printf("\n");
+
 			}
 			sub_populations[i].best_global_fitness = best_global_fitness;
-			// printf("%lf %lf\n", p.fitness, sub_populations[i].best_global_fitness);
 		}
 	}
 }
 
 
-void pso(int index[], int index_length, struct SubPopulation *sub_population, double global_solution[])
+/************************************************************************
+* Function Name : pso
+* Create Date : 2016/11/04
+* Author/Corporation : bigzhao
+**
+Description : 亚种群的pso优化算法，这里默认是CS-L结构
+*
+* Param : index: 亚种群对应全局的下标数组
+* sub_population: 亚种群的指针，指向该次优化的亚种群
+* global_solution： NUM维度的全局解，用来计算适应度
+**
+************************************************************************/
+void pso(int index[], int index_length, SubPopulation *sub_population, double global_solution[])
 {
 	int i, j, k, l;
 	double r1, r2;
@@ -178,39 +250,35 @@ void pso(int index[], int index_length, struct SubPopulation *sub_population, do
 
 	srand((unsigned)time(NULL));
 
-
 	for (i = 0; i < NUM_ITERATION; i++)
 	{
 		for (j = 0; j < NUM_PARTICLE; j++)
 		{
-			// currP = (*sub_population).swarm[j];
-			for(k = 0; k < index_length; k++)
+			for (k = 0; k < index_length; k++)
 			{
 				r1 = rand() / (double)RAND_MAX;
 				r2 = rand() / (double)RAND_MAX;
-
 				new_velocity = (W * (*sub_population).swarm[j].velocity[k]) + 
 				               (C1 * r1 * ((*sub_population).swarm[j].best_position[k] - (*sub_population).swarm[j].position[k])) + 
 				               (C2 * r2 * ((*sub_population).best_global_position[k] - (*sub_population).swarm[j].position[k]));
-				// printf("r1:%lf r2:%lf\n", r1, r2);
-				if(new_velocity < MIN_V)
+
+				if (new_velocity < MIN_V)
 				{
 					new_velocity = MIN_V;
 				}
-
 				else if (MAX_V < new_velocity)
 				{
 					new_velocity = MAX_V;
 				}
 
 				(*sub_population).swarm[j].velocity[k] = new_velocity;
-
 				new_position = (*sub_population).swarm[j].position[k] + new_velocity;
-				if(new_position < MIN_X)
+
+				if (new_position < MIN_X)
 				{
 					new_position = MIN_X;
 				}
-				else if(MAX_X < new_position)
+				else if (MAX_X < new_position)
 				{
 					new_position = MAX_X;
 				}
@@ -222,8 +290,7 @@ void pso(int index[], int index_length, struct SubPopulation *sub_population, do
 			rebulid(index, index_length, (*sub_population).swarm[j].position, global_solution, rebulid_solution);
 			new_fitness = object_function(rebulid_solution);
 
-            //tihuan jubu position
-			if(new_fitness < (*sub_population).swarm[j].best_fitness)
+			if (new_fitness < (*sub_population).swarm[j].best_fitness)
 			{
 				(*sub_population).swarm[j].best_fitness = new_fitness;
 				for (k = 0; k < index_length; k++)
@@ -231,8 +298,8 @@ void pso(int index[], int index_length, struct SubPopulation *sub_population, do
 					(*sub_population).swarm[j].best_position[k] = (*sub_population).swarm[j].position[k];
 				}
 			}
-            // tihuan quanju position
-			if(new_fitness < (*sub_population).best_global_fitness)                
+
+			if (new_fitness < (*sub_population).best_global_fitness)                
 			{
 				(*sub_population).best_global_fitness = new_fitness;
 				for (int k = 0; k < index_length; k++)
@@ -246,34 +313,48 @@ void pso(int index[], int index_length, struct SubPopulation *sub_population, do
 }
 
 
+/************************************************************************
+* Function Name : competition
+* Create Date : 2016/11/04
+* Author/Corporation : bigzhao
+**
+Description : 重叠种群之间的竞争函数，随机抽取每个维度，找出重叠的亚种群，比较适应度得出最好的组成全局解
+*
+* Param : sub_index: 一个SUBPOP_LEN × （L + 1）的二维数组，用来存放每个亚种群对应的下标
+* global_solution： NUM维度的全局解，用来计算适应度
+**
+************************************************************************/
 void competition(int sub_index[SUBPOP_LEN][L + 1], double global_solution[])
 {
 	int i, j, random_array[NUM];
 	double fitness, best_fitness;
 	double best_solution[NUM];
     memcpy(best_solution, global_solution, sizeof(double) * NUM);
+    
     for(i = 0; i < NUM; i++)
     {
     	random_array[i] = i;
     }
 	make_array_random(random_array, NUM);
 
-	for(i = 0; i < NUM; i++)
+	for (i = 0; i < NUM; i++)
     {
     	random_array[i] = i;
-    	// printf("%d\n", random_array[i]);
     }
 	for (i = 0; i < NUM; ++i)
 	{ 
 		best_fitness = object_function(global_solution);
 
-		for(j = 0; j < SUBPOP_LEN; j++)
+		for (j = 0; j < SUBPOP_LEN; j++)
 		{
-			if(judge_in_array(random_array[i], sub_index[j], L + 1))
+			// 找出重叠的亚种群
+			if (judge_in_array(random_array[i], sub_index[j], L + 1))
 			{
 				global_solution[random_array[i]] = sub_populations[sub_index[j][0]].best_global_position[random_array[i] - sub_index[j][0]];
 				fitness = object_function(global_solution);
-				if(fitness < best_fitness)
+
+				// 找出当前维度最好的值组成全局解
+				if (fitness < best_fitness)
 				{
 					best_solution[random_array[i]] = global_solution[random_array[i]];
 					best_fitness = fitness;
@@ -285,60 +366,75 @@ void competition(int sub_index[SUBPOP_LEN][L + 1], double global_solution[])
 }
 
 
+/************************************************************************
+* Function Name : sharing
+* Create Date : 2016/11/04
+* Author/Corporation : bigzhao
+**
+Description : 亚种群之间的分享，经过竞争步骤之后此时全局解为各个亚种群的最优组成，利用全局解反向影响局部。
+找出当前亚种群最差的解用全局解去替换。
+*
+* Param : sub_index: 一个SUBPOP_LEN × （L + 1）的二维数组，用来存放每个亚种群对应的下标
+* global_solution： NUM维度的全局解，用来计算适应度
+**
+************************************************************************/
 void sharing(int sub_indexp[SUBPOP_LEN][L + 1], double global_solution[])
 {
-	int i, j, worst, random_array[NUM -L];
+	int i, j, worst;
 
-    for(i = 0; i < SUBPOP_LEN; i++)
-    {
-    	random_array[i] = i;
-    }
-	make_array_random(random_array, SUBPOP_LEN);
-
-	for(i = 0; i < SUBPOP_LEN; i++)
+	for (i = 0; i < SUBPOP_LEN; i++)
 	{
 		worst = 0;
-		for(j = 1; j < NUM_PARTICLE; j++)
+
+		for (j = 1; j < NUM_PARTICLE; j++)
 		{
-			if(sub_populations[random_array[i]].swarm[worst].fitness < sub_populations[random_array[i]].swarm[j].fitness)
+			if (sub_populations[i].swarm[worst].fitness < sub_populations[i].swarm[j].fitness)
 			{
 				worst = j;
 			}
-
 		}
-		for(j = 0; j < L + 1; j++)
+
+		for (j = 0; j < L + 1; j++)
 		{
-			sub_populations[random_array[i]].swarm[worst].position[j] = global_solution[sub_indexp[random_array[i]][j]];
+			sub_populations[i].swarm[worst].position[j] = global_solution[sub_indexp[i][j]];
 		}
 	}
 }
 
 
+/************************************************************************
+* Function Name : sharing
+* Create Date : 2016/11/04
+* Author/Corporation : bigzhao
+**
+Description : FEA算法，先初始化亚种群，然后进入循环进行亚种群的优化、种群间的竞争和分享。
+终止条件：连续15代无更好结果出现跳出循环，打印结果。
+**
+************************************************************************/
 void fea()
 {
-		int sub_index[SUBPOP_LEN][L + 1], i;
+	int sub_index[SUBPOP_LEN][L + 1], i;
 	int record = 0;
 	double global_solution[NUM], last_fitness;
 	srand((unsigned)time(NULL));
-	// make a global solution
-	for(i = 0; i < NUM; i++)
+	// 随机生成NUM维全局解
+	for (i = 0; i < NUM; i++)
 	{
 		global_solution[i] = (double)(MAX_X - MIN_X) * rand() / (double)RAND_MAX +  MIN_X;
 	}
 
     initialize_subpops(sub_index, sub_populations, global_solution);
-
     last_fitness = object_function(global_solution);
 
     while(1)
     {
-    	for(i = 0; i < SUBPOP_LEN; i++)
+    	for (i = 0; i < SUBPOP_LEN; i++)
     	{
     		pso(sub_index[i], L+1, &sub_populations[i], global_solution);
     	}
     	competition(sub_index, global_solution);
     	sharing(sub_index, global_solution);
-    	if(last_fitness > object_function(global_solution))
+    	if (last_fitness > object_function(global_solution))
     	{
     		record = 0;
     		last_fitness = object_function(global_solution);
@@ -347,10 +443,12 @@ void fea()
     	{
     		record += 1;
     	}
-    	if(15 <= record)
+    	// 连续15代无更好结果
+    	if (15 <= record)
     		break;
     }
 
+    // 打印结果
     printf("the fea run over.\nThe best fitness is %lf \n .The solution is:", last_fitness);
     for (i = 0; i < NUM; i++)
     {
--- fea.h
+++ fea.h
@@ -1,17 +1,18 @@
 #ifndef _FEA_
 #define _FEA_ _FEA_
 
-#define NUM  200//  problem size
-#define NUM_PARTICLE 10 // particle size
-#define NUM_ITERATION 20 // subpopularion iter 20 times
-#define MAX_X 100.0
-#define MIN_X -100.0
-#define MIN_V (-1.0 * MAX_X)
-#define MAX_V (MAX_X)
-#define W 0.729
-#define C1 1.49455
-#define C2 1.49455 
+#define NUM  200                          // 待优化问题的维度 
+#define NUM_PARTICLE 10                   // pso 粒子群的 size
+#define NUM_ITERATION 20                  // 亚种群每次优化迭代的次数
+#define MAX_X 32.0      				  // 最大值 防止越界 依照问题而定
+#define MIN_X -32.0						  // 最小值 防止越界 依照问题而定
+#define MIN_V (-1.0 * MAX_X)              // PSO 的最小速度 
+#define MAX_V (MAX_X)                     // PSO 的最大速度 
+#define W 0.729							  // PSO 的权重 
+#define C1 1.49455                        // PSO 学习因子
+#define C2 1.49455                        // PSO 学习因子
+#define L 2 //this is cs-l                // CS-L 结构的 L
+#define SUBPOP_LEN (NUM - L)              // CS-L 结构的亚种群的size
+#define PI 3.1415926                      // 派
 
-#define L 2 //this is cs-l
-#define SUBPOP_LEN (NUM - L)
 #endif
